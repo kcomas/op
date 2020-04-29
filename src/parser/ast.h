675d25f24cb439da8e4b53bc6fa5166029d3f2b7
kcomas <kevin.comas.git@gmail.com>
2020-04-29 07:37:51 -0400

    ast def

--- src/parser/ast.h
+++ src/parser/ast.h
@@ -6,13 +6,40 @@
 #define AST_PFX(NAME) AST_##NAME
 
 typedef enum {
-    AST_PFX(ARG),
-    AST_PFX(LOCAL),
     AST_PFX(VAR),
+    AST_PFX(LOAD),
+    AST_PFX(UOP),
+    AST_PFX(BOP),
     AST_PFX(FUNCTION),
-    AST_PFX(IF),
     AST_PFX(CALL)
+    AST_PFX(IF),
 } ast_type;
+
+typedef struct _ast_stmt ast_stmt;
+
+#define AST_LOAD_PFX(NAME) AST_LOAD_##NAME
+
+typedef enum {
+    AST_LOAD_PFX(ARG),
+    AST_LOAD_PFX(LOCAL)
+} ast_load_type;
+
+typedef struct {
+    ast_load_type type;
+    int8_t idx;
+    var_string *name;
+} ast_load;
+
+#define AST_UOP_PFX(NAME) AST_UOP_##NAME
+
+typedef enum {
+    AST_UOP_PFX(PUSH),
+} ast_uop_type;
+
+typedef struct {
+    ast_uop_type type;
+    ast_stmt *right;
+} ast_uop;
 
 #define AST_BOP_PFX(NAME) AST_BOP_##NAME
 
@@ -22,27 +49,21 @@ typedef enum {
     AST_BOP_PFX(FILEWRITE)
 } ast_bop_type;
 
-#define AST_UOP_PFX(NAME) AST_UOP_##NAME
-
-typedef enum {
-    AST_UOP_PFX(PUSH)
-} ast_uop_type;
-
-typedef struct _ast_stmt ast_stmt;
-
-typedef struct {
-    ast_uop_type type;
-    ast_stmt *right;
-} ast_uop;
-
-typedef struct {
-    ast_var **target;
-} ast_call;
-
 typedef struct {
     ast_bop_type type;
     ast_stmt *left, *right;
 } ast_bop;
+
+typedef struct _ast_var_list {
+    ast_var *value;
+    struct _ast_var_list *next;
+} ast_var_list;
+
+typedef struct {
+    size_t num_args;
+    ast_var *target;
+    ast_var_list *args;
+} ast_call;
 
 typedef struct {
     ast_stmt *else_stmt;
@@ -56,17 +77,18 @@ typedef struct _ast_fuction ast_function;
 typedef struct _ast_stmt {
     ast_type type;
     union {
-        ast_var **value;
-        ast_function *fn_stmt;
-        ast_if *if_stmt;
-        ast_bop *bop_stmt;
-        ast_uop *uop_stmt;
+        ast_var *value;
+        ast_load *load;
+        ast_bop *bop;
+        ast_uop *uop;
+        ast_function *fn;
         ast_call *call;
+        ast_if *if_stmt;
     } stmt;
-    struct _ast_stmt* next;
+    struct _ast_stmt *next;
 } ast_stmt;
 
 typedef struct _ast_fuction {
-    var_hash *arg_tbl, *local_tbl;
+    var_hash *sym_table;
     ast_stmt *stmt;
 } ast_function;
